6dd926d0dd084623d28dafc6f24bb603e65eeeb9
MMaldonado1017 <miguelmaldonado199@gmail.com>
2023-06-29 14:33:15 -0400

    Deposit Withdrawl And Transaction Controllers Complete Transaction Service Complete

--- src/main/java/com/BankApi/BankApi/service/TransactionService.java
+++ src/main/java/com/BankApi/BankApi/service/TransactionService.java
@@ -24,43 +24,54 @@ public class TransactionService {
     }
 
     public Transaction deposit(Long accountId, Transaction transaction) {
-        Account account = accountRepository.findById(accountId).orElseThrow(
-                () -> new ResourceNotFoundException(String.format("Account with ID %s not found", accountId))
-        );
+        Account account = accountRepository.findById(accountId)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Account with ID %s not found", accountId)));
+
         account.setBalance(account.getBalance() + transaction.getAmount());
         accountRepository.save(account);
+
         transaction.setType(TransactionType.DEPOSIT);
+        transaction.setPayeeAccount(account);
+        transaction.setPayerAccount(null);
         return transactionRepository.save(transaction);
     }
 
     public Transaction withdraw(Long accountId, Transaction transaction) {
-        Account account = accountRepository.findById(accountId).orElseThrow(
-                () -> new ResourceNotFoundException(String.format("Account with ID %s not found", accountId))
-        );
-        account.setBalance(account.getBalance() - transaction.getAmount());
+        Account account = accountRepository.findById(accountId)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Account with ID %s not found", accountId)));
+
+        double withdrawalAmount = transaction.getAmount();
+        if (withdrawalAmount > account.getBalance()) {
+            throw new InsufficientFundsException("Insufficient funds in the account for withdrawal");
+        }
+
+        account.setBalance(account.getBalance() - withdrawalAmount);
         accountRepository.save(account);
+
         transaction.setType(TransactionType.WITHDRAWAL);
+        transaction.setPayeeAccount(null);
+        transaction.setPayerAccount(account);
         return transactionRepository.save(transaction);
     }
 
     public List<Transaction> getAllDepositsByAccountId(Long accountId) {
-        Account account = accountRepository.findById(accountId).orElseThrow(
-                () -> new ResourceNotFoundException(String.format("Account with ID %s not found", accountId))
-        );
+        Account account = accountRepository.findById(accountId)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Account with ID %s not found", accountId)));
+
         return transactionRepository.findAllDepositsByAccountId(accountId);
     }
 
     public List<Transaction> getAllWithdrawalsByAccountId(Long accountId) {
-        Account account = accountRepository.findById(accountId).orElseThrow(
-                () -> new ResourceNotFoundException(String.format("Account with ID %s not found", accountId))
-        );
+        Account account = accountRepository.findById(accountId)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Account with ID %s not found", accountId)));
+
         return transactionRepository.findAllWithdrawalsByAccountId(accountId);
     }
 
     public List<Transaction> getAllTransactionsByAccountId(Long accountId) {
-        Account account = accountRepository.findById(accountId).orElseThrow(
-                () -> new ResourceNotFoundException(String.format("Account with ID %s not found", accountId))
-        );
+        Account account = accountRepository.findById(accountId)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Account with ID %s not found", accountId)));
+
         return transactionRepository.findAllTransactionsByAccountId(accountId);
     }
 
@@ -72,67 +83,25 @@ public class TransactionService {
         return transactionRepository.findAllWithdrawals();
     }
 
-    public List<Transaction> getAllTransactions() {
-        return transactionRepository.findAll();
-    }
-
     public Transaction getDepositById(Long id) {
-        Transaction transaction = transactionRepository.findById(id).orElseThrow(
-                () -> new ResourceNotFoundException(String.format("Deposit with ID %s not found", id))
-        );
-        if (!transaction.getType().equals(TransactionType.DEPOSIT)) {
-            throw new RuntimeException(String.format("Transaction with ID %s is not a Deposit", id));
+        Transaction deposit = transactionRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Deposit with ID %s not found", id)));
+
+        if (deposit.getType() != TransactionType.DEPOSIT) {
+            throw new IllegalArgumentException(String.format("Transaction with ID %s is not a Deposit", id));
         }
-        return transaction;
+
+        return deposit;
     }
 
     public Transaction getWithdrawalById(Long id) {
-        Transaction transaction = transactionRepository.findById(id).orElseThrow(
-                () -> new ResourceNotFoundException(String.format("Withdrawal with ID %s not found", id))
-        );
-        if (!transaction.getType().equals(TransactionType.WITHDRAWAL)) {
-            throw new RuntimeException(String.format("Transaction with ID %s is not a Withdrawal", id));
+        Transaction withdrawal = transactionRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Withdrawal with ID %s not found", id)));
+
+        if (withdrawal.getType() != TransactionType.WITHDRAWAL) {
+            throw new IllegalArgumentException(String.format("Transaction with ID %s is not a Withdrawal", id));
         }
-        return transaction;
-    }
 
-    public Transaction getTransactionById(Long id) {
-        return transactionRepository.findById(id).orElseThrow(
-                () -> new ResourceNotFoundException(String.format("Transaction with ID %s not found", id))
-        );
-    }
-
-    public void deleteDeposit(Long depositId) {
-        transactionRepository.deleteById(depositId);
-    }
-
-    public void deleteWithdrawal(Long withdrawalId) {
-        transactionRepository.deleteById(withdrawalId);
-    }
-
-    public Transaction updateDeposit(Long depositId, Transaction updatedTransaction) {
-        Transaction transaction = transactionRepository.findById(depositId).orElseThrow(
-                () -> new ResourceNotFoundException(String.format("Deposit with ID %s not found", depositId))
-        );
-
-        transaction.setAmount(updatedTransaction.getAmount());
-        transaction.setDate(updatedTransaction.getDate());
-        // Update any other properties if necessary
-        // ...
-
-        return transactionRepository.save(transaction);
-    }
-
-    public Transaction updateWithdrawal(Long withdrawalId, Transaction updatedTransaction) {
-        Transaction transaction = transactionRepository.findById(withdrawalId).orElseThrow(
-                () -> new ResourceNotFoundException(String.format("Withdrawal with ID %s not found", withdrawalId))
-        );
-
-        transaction.setAmount(updatedTransaction.getAmount());
-        transaction.setDate(updatedTransaction.getDate());
-        // Update any other properties if necessary
-        // ...
-
-        return transactionRepository.save(transaction);
+        return withdrawal;
     }
 }
