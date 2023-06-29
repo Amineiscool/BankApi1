fdcddbee714b9529cf1fa6f724e0e58185ca7966
MMaldonado1017 <miguelmaldonado199@gmail.com>
2023-06-29 16:14:38 -0400

    Uncommented comments completed tasks

--- src/main/java/com/BankApi/BankApi/controller/transaction/TransactionController.java
+++ src/main/java/com/BankApi/BankApi/controller/transaction/TransactionController.java
@@ -1,35 +1,35 @@
 package com.BankApi.BankApi.controller.transaction;
 
 
-//import com.BankApi.BankApi.model.Transaction;
-//import com.BankApi.BankApi.service.TransactionService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//import java.util.ListResourceBundle;
-//@RestController
-//public class TransactionController {
-//
-//    private final TransactionService transactionService;
-//
-//    @Autowired
-//    public TransactionController(TransactionService transactionService) {
-//        this.transactionService = transactionService;
-//    }
-//
-//    @GetMapping("/accounts/{accountId}/transactions")
-//    public ResponseEntity<List<Transaction>> getAllTransactionsByAccountId(@PathVariable Long accountId) {
-//        return new ResponseEntity<>(transactionService.getAllTransactionsByAccountId(accountId), HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/transactions/{transactionId}")
-//    public ResponseEntity<Transaction> getTransactionById(@PathVariable Long transactionId) {
-//        return new ResponseEntity<>(transactionService.getTransactionById(transactionId), HttpStatus.OK);
-//    }
-//}
+import com.BankApi.BankApi.model.Transaction;
+import com.BankApi.BankApi.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.ListResourceBundle;
+@RestController
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @Autowired
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping("/accounts/{accountId}/transactions")
+    public ResponseEntity<List<Transaction>> getAllTransactionsByAccountId(@PathVariable Long accountId) {
+        return new ResponseEntity<>(transactionService.getAllTransactionsByAccountId(accountId), HttpStatus.OK);
+    }
+
+    @GetMapping("/transactions/{transactionId}")
+    public ResponseEntity<Transaction> getTransactionById(@PathVariable Long transactionId) {
+        return new ResponseEntity<>(transactionService.getTransactionById(transactionId), HttpStatus.OK);
+    }
+}
 
